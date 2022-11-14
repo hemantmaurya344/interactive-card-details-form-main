3172a13d35603c4dca4a4ac7536f00953f33591c
Hemant Maurya <hemantmaurya344@gmail.com>
2022-11-14 18:36:43 +0530

    5th change

---
+++ index.js
@@ -0,0 +1,5 @@
+let expMonth = document.getElementById('form-month').addEventListener('click',() => {
+    let expMonthContent = e.value;
+    console.log('expMonthContent');
+    alert(expMonthContent);
+});
